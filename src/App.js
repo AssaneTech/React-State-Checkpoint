a31032cb020dab7f5c9aa6cf54d21e0e875e0f2e
AssaneTech <assanediouf0063gmail.com>
2025-06-15 12:39:17 +0000

    Add a button that toggles the show state. When the show state is true, the person's profile will appear.

--- src/App.js
+++ src/App.js
@@ -9,20 +9,32 @@ constructor(){
       bio :" A passionate software engineer",
       imgSrc : "./images/MyProfile.png",
       profession:"Software Engineer",
-
-    }
+    },
+    shows:true,
   }
 }
-
+toggleShow = () => {
+    this.setState((prevState) => ({
+      shows: !prevState.shows, // Toggle true/false
+    }));
+  };
 
   render (){
     return (
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <button onClick={this.toggleShow}>
+          {this.state.shows ? "Hide Profile" : "Show Profile"}
+        </button>
+        {this.state.shows && (
+
       <div>
         <h1>React Class Component with State</h1>
         <h2>{this.state.person.fullName}</h2>
         <p>{this.state.person.bio}</p>
         <img src={this.state.person.imgSrc} alt="Assane Profile" />
         <h3>{this.state.person.profession}</h3>
+      </div>
+       )}
       </div>
     );
   }
