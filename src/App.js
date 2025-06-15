e44a5b69308686fd4f947034993868f8a44ea738
AssaneTech <assanediouf0063gmail.com>
2025-06-15 12:16:17 +0000

    Implement a state for this class containing a Person ={ fullName,bio, imgSrc, profession} and a boolean shows.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, {Component} from "react";
 
 class App extends Component{
+constructor(){
+  super();
+  this.state={
+    person:{
+      fullName : "Assane DIOUF",
+      bio :" A passionate software engineer",
+      imgSrc : "./images/MyProfile.png",
+      profession:"Software Engineer",
+
+    }
+  }
+}
+
+
   render (){
     return (
-    <div>
-      <h1> This a react component</h1>
-    </div>)
+      <div>
+        <h1>React Class Component with State</h1>
+        <h2>{this.state.person.fullName}</h2>
+        <p>{this.state.person.bio}</p>
+        <img src={this.state.person.imgSrc} alt="Assane Profile" />
+        <h3>{this.state.person.profession}</h3>
+      </div>
+    );
   }
 }
 
